ae32fe1cd2da38138d5632d5f612d7a86f6c0f90
Fede <fedemc@gmail.com>
2017-11-07 22:53:38 -0300

    Agregados Commands para Attack, MoveLeft, MoveRight, Jump, Idle. InputHandler detecta del joystick y ejecuta los commands respectivos.

---
+++ Assets/Code/Controls/Command.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class Command
+{
+    public abstract void Execute(GameObject obj, Command comm);
+}
---
+++ Assets/Code/Controls/Commands/MoveRight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public  class MoveRight : Command
+{
+    public override void Execute(GameObject obj, Command comm)
+    {
+        MoveToRight(obj);
+    }
+
+
+    void MoveToRight(GameObject obj)
+    {
+        //Debug.Log("Moving right");
+        var movSpeed = obj.GetComponent<MoveStats>().MoveSpeed;     //Voy a las move stats(script) del objeto y me traigo el valor del move speed
+        obj.GetComponent<MoveStats>().FacingRight = true;           //Cambio el state de FacingRight a true porque estoy mirando hacia la derecha
+        obj.GetComponent<Animator>().SetBool("isWalking", true);    //Seteo la animacion de caminar
+        obj.GetComponentInChildren<SpriteRenderer>().flipX = false;
+        obj.transform.Translate(Vector2.right * movSpeed * Time.deltaTime);  //Aplico movimiento al objeto
+    }
+
+}
